fc8d9212eac70b120c7b807a2a41d6dc85e0acec
HouBinyang <21302010042@m.fudan.edu.cn>
2024-03-27 21:19:02 +0800

    feat: index tests

---
+++ src/test/java/com/demo/index/IndexControllerTest.java
@@ -0,0 +1,106 @@
+package com.demo.index;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import com.demo.controller.IndexController;
+import com.demo.entity.Message;
+import com.demo.entity.News;
+import com.demo.entity.Venue;
+import com.demo.entity.vo.MessageVo;
+import com.demo.service.MessageService;
+import com.demo.service.MessageVoService;
+import com.demo.service.NewsService;
+import com.demo.service.VenueService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.*;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebMvcTest(IndexController.class)
+public class IndexControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private NewsService newsService;
+
+    @MockBean
+    private VenueService venueService;
+
+    @MockBean
+    private MessageVoService messageVoService;
+
+    @MockBean
+    private MessageService messageService;
+
+    @Test
+    public void testIndex() throws Exception {
+        List<News> newsList = new ArrayList<>();
+        List<Venue> venueList = new ArrayList<>();
+        List<MessageVo> messageVoList = new ArrayList<>();
+        List<Message> messageList = new ArrayList<>();
+        Page<Message> messagePage = new PageImpl<>(messageList);
+
+        // empty info
+        when(newsService.findAll(any(Pageable.class))).thenReturn(new PageImpl<>(newsList));
+        when(venueService.findAll(any(Pageable.class))).thenReturn(new PageImpl<>(venueList));
+        when(messageService.findPassState(any(Pageable.class))).thenReturn(messagePage);
+        when(messageVoService.returnVo(anyList())).thenReturn(messageVoList);
+
+        mockMvc.perform(get("/index"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"))
+                .andExpect(model().attribute("user", (String) null))
+                .andExpect(model().attribute("news_list", newsList))
+                .andExpect(model().attribute("venue_list", venueList))
+                .andExpect(model().attribute("message_list", messageVoList));
+
+        // not empty info
+        newsList.add(new News(1, "title", "content", LocalDateTime.now()));
+        newsList.add(new News(2, "title", "content", LocalDateTime.now()));
+        venueList.add(new Venue(1, "venue_name", "description", 1, "picture", "address", "open_time", "close_time"));
+        messageVoList.add(new MessageVo(1,"title","content",LocalDateTime.now(),"username","email",1));
+        messageList.add(new Message(1,"userID","content",LocalDateTime.now(),1));
+        messagePage = new PageImpl<>(messageList);
+
+        when(newsService.findAll(any(Pageable.class))).thenReturn(new PageImpl<>(newsList));
+        when(venueService.findAll(any(Pageable.class))).thenReturn(new PageImpl<>(venueList));
+        when(messageService.findPassState(any(Pageable.class))).thenReturn(messagePage);
+        when(messageVoService.returnVo(anyList())).thenReturn(messageVoList);
+
+        mockMvc.perform(get("/index"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"))
+                .andExpect(model().attribute("user", (String) null))
+                .andExpect(model().attribute("news_list", newsList))
+                .andExpect(model().attribute("venue_list", venueList))
+                .andExpect(model().attribute("message_list", messageVoList));
+
+        // null info
+        // bug here
+        // null check should be added
+//        when(newsService.findAll(any(Pageable.class))).thenReturn(null);
+//        when(venueService.findAll(any(Pageable.class))).thenReturn(null);
+//        when(messageService.findPassState(any(Pageable.class))).thenReturn(null);
+//        when(messageVoService.returnVo(anyList())).thenReturn(null);
+//
+//        mockMvc.perform(get("/index"))
+//                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testAdminIndex() throws Exception {
+        mockMvc.perform(get("/admin_index"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("admin/admin_index"));
+    }
+}
