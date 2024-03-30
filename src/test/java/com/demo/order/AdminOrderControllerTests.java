f0c868189218cb4b40f5949b64a5c3cb72cbb73c
Suzyyy <suzyyy@SuzyyydeMacBook-Pro.local>
2024-03-30 23:09:20 +0800

    add: adminController getOrderList

--- src/test/java/com/demo/order/AdminOrderControllerTests.java
+++ src/test/java/com/demo/order/AdminOrderControllerTests.java
@@ -13,6 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.demo.controller.admin.AdminOrderController;
 import com.demo.dao.OrderDao;
 import com.demo.entity.Order;
+import com.demo.entity.User;
 import com.demo.entity.vo.OrderVo;
 import com.demo.service.OrderService;
 import com.demo.service.OrderVoService;
@@ -93,6 +94,63 @@ public class AdminOrderControllerTests {
                 .andExpect(view().name("admin/reservation_manage"))
                 .andExpect(model().attribute("order_list",mockOrderVos))
                 .andExpect(model().attribute("total",mockEmptyPage.getTotalPages()));
+    }
+
+    @Test
+    public void testAdminGetOrderListWithValidPage() throws Exception{
+        // mock orders
+        List<Order> mockOrderList = new ArrayList<>();
+        mockOrderList.add(new Order());
+        List<OrderVo> mockVo = new ArrayList<>();
+
+        // mock page
+        Page<Order> pageOfManyOrders = new PageImpl<>(mockOrderList);
+
+        //mock service
+        int pageNormal = 0;
+        Pageable orderPageableNormal = PageRequest.of(pageNormal,10, Sort.by("orderTime").descending());
+        when(orderService.findNoAuditOrder(orderPageableNormal)).thenReturn(pageOfManyOrders);
+        when(orderVoService.returnVo(mockOrderList)).thenReturn(mockVo);
+
+        // test
+        mockMvc.perform(get("/admin/getOrderList.do"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$").value(mockVo));
+    }
+
+    @Test
+    public void testGetOrderListWithErrorPageMin() throws Exception {
+        // bug here
+        // 没有做-1输入的处理，导致PageRequest.of()失败
+        mockMvc.perform(get("/admin/getOrderList.do").param("page", "-1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").doesNotExist());
+    }
+
+    @Test
+    public void testGetOrderListWithErrorPageMax() throws Exception {
+        // bug here
+        // 没有做page越界处理，导致null对象调用
+        User user = new User();
+        user.setUserID("nct127");
+
+        Pageable order_pageable = PageRequest.of(5-1,5, Sort.by("orderTime").descending());
+        when(orderService.findNoAuditOrder(order_pageable)).thenReturn(null);
+        //when(orderVoService.returnVo(mockOrderList)).thenReturn(mockVo);
+
+        mockMvc.perform(get("/admin/getOrderList.do").param("page","5"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").doesNotExist());
+    }
+
+    @Test
+    public void testGetOrderListWithStringPage() throws Exception {
+        User user = new User();
+        user.setUserID("nct127");
+        mockMvc.perform(get("/admin/getOrderList.do").param("page","jw"))
+                .andExpect(status().isBadRequest());
     }
 
     @Test
