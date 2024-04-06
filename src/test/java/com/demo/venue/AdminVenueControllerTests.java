75e208da846626c352d1d033eec987e32bfb71e0
polaris <2673986789@qq.com>
2024-04-06 11:49:47 +0800

    fix: testAddVenueWithConflict

--- src/test/java/com/demo/venue/AdminVenueControllerTests.java
+++ src/test/java/com/demo/venue/AdminVenueControllerTests.java
@@ -21,7 +21,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
-import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
