505ebd67a7d4976c6014c2f7d20f0b91dd4903fd
HouBinyang <21302010042@m.fudan.edu.cn>
2024-05-16 14:29:27 +0800

    feat: base mutator

--- simple_fuzzer/utils/Mutator.py
+++ simple_fuzzer/utils/Mutator.py
@@ -5,33 +5,74 @@ from typing import Any
 import string
 
 
-def insert_random_character(s: str) -> str:
+def insert_random_character(s: str, print_mutator=False) -> str:
     """
     向 s 中下标为 pos 的位置插入一个随机 byte
     pos 为随机生成，范围为 [0, len(s)]
     插入的 byte 为随机生成，范围为 [32, 127]
     """
+    # 随机生成 pos 和 byte
     pos = random.randint(0, len(s))
     random_char = chr(random.randint(32, 127))
+
+    if print_mutator:
+        print(f'\nInsert {random_char} at {pos}')
+
     return s[:pos] + random_char + s[pos:]
 
-def flip_random_bits(s: str) -> str:
+
+def flip_random_bits(s: str, print_mutator=False) -> str:
     """
     基于 AFL 变异算法策略中的 bitflip 与 random havoc 实现相邻 N 位翻转（N = 1, 2, 4），其中 N 为随机生成
     从 s 中随机挑选一个 bit，将其与其后面 N - 1 位翻转（翻转即 0 -> 1; 1 -> 0）
     注意：不要越界
     """
+    # 随机生成 N
     N = random.choice([1, 2, 4])
-    # 随机选择一个索引位置
-    index = random.randint(0, len(s) - N)
-    # 对索引位置及其后面的 N - 1 位进行翻转
-    result = list(s)
-    for i in range(N):
-        result[index + i] = '1' if result[index + i] == '0' else '0'
-    return ''.join(result)
+    # 随机选择一个 bit
+    random_index = random.randint(0, 8 * (len(s) - N))
+
+    # 计算 bit 所在的字符索引和 bit 索引
+    char_index = random_index // 8
+    bit_index = random_index % 8
+
+    # 获取 char_index 和 char_index + 1 处的字符
+    char_1 = s[char_index]
+    char_2 = s[char_index + 1] if char_index + 1 < len(s) else None
+    ascii_char_1 = ord(char_1)
+    ascii_char_2 = ord(char_2) if char_2 is not None else None
+
+    # 将char_1 bit_index 及后的 N - 1 位进行翻转
+    if bit_index + N < 8:
+        for i in range(N):
+            ascii_char_1 ^= 1 << (bit_index + i)
+    else:
+        for i in range(8 - bit_index):
+            ascii_char_1 ^= 1 << (bit_index + i)
+
+        if char_2 is not None:
+            for i in range(N - 8 + bit_index):
+                ascii_char_2 ^= 1 << i
+
+    flipped_char_1 = chr(ascii_char_1)
+    flipped_char_2 = chr(ascii_char_2) if char_2 is not None else None
+
+    if flipped_char_2 is not None:
+        result = s[:char_index] + flipped_char_1 + flipped_char_2
+        if char_index + 2 < len(s):
+            result += s[char_index + 2:]
+    else:
+        result = s[:char_index] + flipped_char_1
+        if char_index + 1 < len(s):
+            result += s[char_index + 1:]
+
+    if print_mutator:
+        print(f'\n{s[char_index:char_index + N]} -> {"".join(result[char_index:char_index + N])}')
+
+    return result
 
 
-def arithmetic_random_bytes(s: str) -> str:
+def arithmetic_random_bytes(s: str, print_mutator=False) -> str:
     """
     基于 AFL 变异算法策略中的 arithmetic inc/dec 与 random havoc 实现相邻 N 字节随机增减（N = 1, 2, 4），其中 N 为随机生成
     字节随机增减：
@@ -41,23 +82,26 @@ def arithmetic_random_bytes(s: str) -> str:
     从 s 中随机挑选一个 byte，将其与其后面 N - 1 个 bytes 进行字节随机增减
     注意：不要越界；如果出现单个字节在添加随机数之后，可以通过取模操作使该字节落在 [0, 255] 之间
     """
-    # TODO
     # 随机生成 N
     N = random.choice([1, 2, 4])
     # 随机选择一个索引位置
     index = random.randint(0, len(s) - N)
     # 从字符串中获取 N 字节，并将其转换为数字
-    bytes_to_modify = [ord(byte) for byte in s[index:index+N]]
+    bytes_to_modify = [ord(byte) for byte in s[index:index + N]]
     # 随机生成增减量
     delta = random.randint(-35, 35)
     # 对选中的字节进行增减操作
     modified_bytes = [(byte + delta) % 256 for byte in bytes_to_modify]
     # 将修改后的字节重新组合成字符串
-    result = s[:index] + ''.join(chr(byte) for byte in modified_bytes) + s[index+N:]
+    result = s[:index] + ''.join(chr(byte) for byte in modified_bytes) + s[index + N:]
+
+    if print_mutator:
+        print(f'\n{s[index:index + N]} -> {"".join(result[index:index + N])}')
+
     return result
 
 
-def interesting_random_bytes(s: str) -> str:
+def interesting_random_bytes(s: str, print_mutator=False) -> str:
     """
     基于 AFL 变异算法策略中的 interesting values 与 random havoc 实现相邻 N 字节随机替换为 interesting_value（N = 1, 2, 4），其中 N 为随机生成
     interesting_value 替换：
@@ -65,73 +109,86 @@ def interesting_random_bytes(s: str) -> str:
         2. 随机挑选 s 中相邻连续的 1, 2, 4 bytes，将其替换为相应 interesting_value 数组中的随机元素；
     注意：不要越界
     """
-    # TODO
     interesting_values = {
         1: [0, 1, 127, 128, 255],  # 对于1 byte
-        2: [0, 1, 256, 65535],      # 对于2 bytes
-        4: [0, 1, 65536, 4294967295]# 对于4 bytes
+        2: [0, 1, 256, 65535],  # 对于2 bytes
+        4: [0, 1, 65536, 4294967295]  # 对于4 bytes
     }
-    
+
     # 随机生成 N
     N = random.choice([1, 2, 4])
-    
+
     # 随机选择一个索引位置
     index = random.randint(0, len(s) - N)
-    
+
     # 随机替换相邻的 N 字节为 interesting_value 数组中的随机元素
+    interesting_value = random.choice(interesting_values[N])
+
     result = list(s)
     for i in range(N):
-        interesting_value = random.choice(interesting_values[N])
-        result[index + i] = chr(interesting_value)
+        interesting_byte = interesting_value % 256
+        result[index + i] = chr(interesting_byte)
+
+        interesting_value >>= 8
+
+    if print_mutator:
+        print(f'\n{s[index:index + N]} -> {"".join(result[index:index + N])}')
+
     return ''.join(result)
 
 
-def havoc_random_insert(s: str):
+def havoc_random_insert(s: str, print_mutator=False):
     """
     基于 AFL 变异算法策略中的 random havoc 实现随机插入
     随机选取一个位置，插入一段的内容，其中 75% 的概率是插入原文中的任意一段随机长度的内容，25% 的概率是插入一段随机长度的 bytes
     """
-    # TODO
     index = random.randint(0, len(s))
-    
+
     # 75% 的概率插入原文中的任意一段随机长度的内容
     if random.random() < 0.75:
         # 随机选择插入内容的起始位置和长度
-        start = random.randint(0, len(s))
+        start = random.randint(0, len(s) - 1)
         length = random.randint(1, len(s) - start)
-        insert_content = s[start:start+length]
+        insert_content = s[start:start + length]
     else:
         # 25% 的概率插入一段随机长度的字节
-        length = random.randint(1, 10)  # 假设长度范围为 1 到 10
+        length = random.randint(1, len(s))  # 长度范围为 1 到 原文长度
         insert_content = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
-    
+
     # 将插入内容插入到原字符串中
     result = s[:index] + insert_content + s[index:]
+
+    if print_mutator:
+        print(f'\nInsert {insert_content} at {index}')
+
     return result
 
 
-
-def havoc_random_replace(s: str):
+def havoc_random_replace(s: str, print_mutator=False):
     """
     基于 AFL 变异算法策略中的 random havoc 实现随机替换
     随机选取一个位置，替换随后一段随机长度的内容，其中 75% 的概率是替换为原文中的任意一段随机长度的内容，25% 的概率是替换为一段随机长度的 bytes
     """
-    # TODO
-    index = random.randint(0, len(s))
-    
+    replace_len = random.randint(1, len(s))
+    index = random.randint(0, len(s) - replace_len)
+
     # 75% 的概率替换为原文中的任意一段随机长度的内容
     if random.random() < 0.75:
         # 随机选择替换内容的起始位置和长度
-        start = random.randint(0, len(s))
-        length = random.randint(1, len(s) - start)
-        replace_content = s[start:start+length]
+        replace_choice_len = random.randint(1, len(s))
+        start = random.randint(0, len(s) - replace_choice_len)
+        replace_content = s[start:start + replace_choice_len]
     else:
         # 25% 的概率替换为一段随机长度的字节
-        length = random.randint(1, 10)  # 假设长度范围为 1 到 10
-        replace_content = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
-    
+        replace_choice_len = random.randint(1, len(s))
+        replace_content = ''.join(random.choices(string.ascii_letters + string.digits, k=replace_choice_len))
+
     # 将替换内容替换原字符串中的一段内容
-    result = s[:index] + replace_content + s[index+len(replace_content):]
+    result = s[:index] + replace_content + s[index + replace_len:]
+
+    if print_mutator:
+        print(f'\n{s[index:index + replace_len]} -> {replace_content}')
+
     return result
 
 
@@ -151,3 +208,20 @@ class Mutator:
     def mutate(self, inp: Any) -> Any:
         mutator = random.choice(self.mutators)
         return mutator(inp)
+
+
+# for test
+if __name__ == '__main__':
+    for i in range(10000):
+        print("\n====================================================")
+        origin = "abcdefgh."
+        print("* Origin:                    ", origin)
+        print("* Insert Random Character:   ", insert_random_character(origin, True))
+        print("* Flip Random Bits:          ", flip_random_bits(origin, True))
+        print("* Arithmetic Random Bytes:   ", arithmetic_random_bytes(origin, True))
+        print("* Interesting Random Bytes:  ", interesting_random_bytes(origin, True))
+        print("* Havoc Random Insert:       ", havoc_random_insert(origin, True))
+        print("* Havoc Random Replace:      ", havoc_random_replace(origin, True))
+        print("====================================================")
+
+    print("\nNo running error for 10000 cases\n\npass. ")
